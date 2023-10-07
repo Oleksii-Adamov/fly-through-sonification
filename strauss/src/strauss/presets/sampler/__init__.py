e39f70c0c56e0ccf2e6e39df7f9162fd26158e71
Max Havryliuk <12u18m29mm@gmail.com>
2023-10-07 18:39:08 +0300

    make strauss ordinary folder

---
+++ strauss/src/strauss/presets/sampler/__init__.py
@@ -0,0 +1,38 @@
+import glob
+import yaml
+import os
+
+#thisdir = '/'.join(__file__.split('/')[:-1])
+thisdir = os.path.dirname(os.path.abspath(__file__))
+
+
+def read_yaml(filename):
+    with open(filename, 'r') as fdata:
+        try:
+            yamldict = yaml.safe_load(fdata)
+        except yaml.YAMLError as err:
+            print(err)
+    return yamldict
+
+def load_ranges(name="default"):
+    filename = f"{thisdir}/ranges/default.yml"
+    return read_yaml(filename)
+
+def load_preset(name="default"):
+    if '/' in name:
+        # if open user directly
+        filename = name
+    else:
+        # else load built-in preset of that name
+        filename = f"{thisdir}/{name}.yml"
+    return read_yaml(filename)
+
+def preset_details(name='*'):
+    pres = glob.glob(f"{thisdir}/*{name.lower()}*.yml")
+    for p in pres:
+        with open(p, 'r') as fdata:
+            try:
+                d = yaml.safe_load(fdata)
+                print(f"\033[1m{d['name']}:\033[0m\n{d['description']}\n")
+            except:
+                pass
