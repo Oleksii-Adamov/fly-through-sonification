c7f8ea797cd43511ff2682f4d1bea88d76fb382b
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2023-10-07 12:42:35 +0300

    changing to sort

--- deepsort_tracking.py
+++ deepsort_tracking.py
@@ -8,6 +8,7 @@ from deep_sort_realtime.deep_sort import nn_matching
 from deep_sort_realtime.deep_sort.detection import Detection
 import numpy as np
 # from deep_sort_realtime.deepsort_tracker import DeepSort
+from sort.sort import Sort
 
 
 class Tracker:
@@ -17,39 +18,44 @@ class Tracker:
     lost_tracks = []
 
     def __init__(self):
-        max_cosine_distance = 0.4
-        nn_budget = None
-
-        encoder_model_filename = 'model_data/mars-small128.pb'
-
-        metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
-        self.tracker = DeepSortTracker(metric, max_age=0)
-        self.encoder = gdet.create_box_encoder(encoder_model_filename, batch_size=1)
+        # max_cosine_distance = 0.4
+        # nn_budget = None
+        #
+        # encoder_model_filename = 'model_data/mars-small128.pb'
+        #
+        # metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+        # self.tracker = DeepSortTracker(metric, max_age=0)
+        # self.encoder = gdet.create_box_encoder(encoder_model_filename, batch_size=1)
 
         # self.tracker = DeepSort(max_age=0)
+
+        self.tracker = Sort()
 
     def update(self, frame, detections):
 
         if len(detections) == 0:
-            self.tracker.predict()
+            # self.tracker.predict()
             self.tracker.update([])
-            self.update_tracks()
+            # self.update_tracks()
             return
 
         bboxes = np.asarray([d[:-1] for d in detections])
         bboxes[:, 2:] = bboxes[:, 2:] - bboxes[:, 0:2]
         scores = [d[-1] for d in detections]
 
-        features = self.encoder(frame, bboxes)
+        # features = self.encoder(frame, bboxes)
 
-        dets = []
-        for bbox_id, bbox in enumerate(bboxes):
-            dets.append(Detection(bbox, scores[bbox_id], features[bbox_id]))
+        # dets = []
+        # for bbox_id, bbox in enumerate(bboxes):
+        #     dets.append(Detection(bbox, scores[bbox_id], features[bbox_id]))
 
-        self.tracker.predict()
-        self.tracker.update(dets)
-        # self.tracker.update_tracks(bboxes, frame)
-        self.update_tracks()
+        # self.tracker.predict()
+        # self.tracker.update(dets)
+        # # self.tracker.update_tracks(bboxes, frame)
+        # self.update_tracks()
+        detections = np.array(detections)
+        # print(detections)
+        track_bbs_ids = self.tracker.update(detections)
 
     def update_tracks(self):
         tracks = []
