7b43af903f361c9e308f177af08706ff1f761265
Max Havryliuk <12u18m29mm@gmail.com>
2023-10-08 22:57:18 +0300

    working version

--- main.py
+++ main.py
@@ -1,75 +1,7 @@
 import cv2
 from analyze_video import track_objects
 from sort_tracking import SortTracker
-from sonification.sonification import SonificationTools
-
-
-def sonificate_video(objects_from_video, frames_to_go_static, vid_w, vid_h, length):
-    #prepare data
-
-    static_small_stars = []
-    static_big_stars = []
-    dynamic_stars = []
-    nebulae = []
-    planets = []
-    is_any_small = False
-    is_any_big = False
-    is_any_dyn = False
-    is_any_neb = False
-    is_any_plan = False
-
-    for pair in objects_from_video:
-        frame_objects = pair[0]
-        frame_n = pair[1]
-
-        #prepare static stars and planets
-        if frame_n % frames_to_go_static == 0:
-            for star in frame_objects['stars']:
-                if star.diameter is None:
-                    is_any_small = True
-                    static_small_stars.append([star, frame_n])
-                else:
-                    is_any_big = True
-                    static_big_stars.append([star, frame_n])
-            for planet in frame_objects['planets']:
-                is_any_plan = True
-                planets.append([planet, frame_n])
-
-        #prepare dynamic stars
-        for star in frame_objects['stars_went_offscreen']:
-            is_any_dyn = True
-            dynamic_stars.append([star, frame_n])
-
-        #prepare nebulae
-        for point in frame_objects['nebulae']:
-            is_any_neb = True
-            nebulae.append([point, frame_n])
-
-
-
-    sss = SonificationTools(vid_w, vid_h, length)
-
-    chords_small_static = [["C4", "C5", "C6"]]
-    chords_big_static = [["A3", "A4", "C5", "A6"]]
-    chords_dynamic = [["A3", "A4", "E4", "C5"]]
-    chords_nebulae = [["A4", "A5", "C5", "E5", "C6"]]
-    chords_planets = [["C3", "E3", "C4", "E4"]]
-
-    #sonification
-    if is_any_small:
-        sss.sonificate_small_stars(sss.get_data_from_small_stars_in_list(static_small_stars, frames_to_go_static, 1), chords_small_static)
-    if is_any_big:
-        sss.sonificate_big_stars(sss.get_data_from_big_stars_in_list(static_big_stars, frames_to_go_static), chords_big_static)
-    if is_any_dyn:
-        sss.sonificate_stars_went_offscreen(sss.get_data_from_dynamyc_stars_in_list(dynamic_stars, 1), chords_dynamic, 0.2)
-    if is_any_neb:
-        sss.sonificate_nebulae_point_list(sss.get_data_from_nebulas_in_list(nebulae), chords_nebulae, 0.2)
-    if is_any_plan:
-        sss.sonificate_planets(sss.get_data_from_planets_in_list(planets, frames_to_go_static), chords_planets, 0.1)
-
-    #mix audiostreams
-    sss.mix_all()
-
+from sonification.sonification import sonificate_video
 
 if __name__ == '__main__':
     visualize = True
@@ -82,7 +14,7 @@ if __name__ == '__main__':
 
     tracked_objects = {}
     tracker = SortTracker()
-    frames_to_go_static = 50
+    frames_to_go_static = 150
     is_dynamic = True
     objects_from_video = []
     frame_number = 0
--- sonification/sonification.py
+++ sonification/sonification.py
@@ -1,5 +1,6 @@
 import os
 import random
+import colorsys
 
 from .strauss.score import Score
 from .strauss.sources import Events, Objects
@@ -11,21 +12,82 @@ import os
 from pydub import AudioSegment
 from .instruments import *
 
+def sonificate_video(objects_from_video, frames_to_go_static, vid_w, vid_h, length):
+    #prepare data
 
+    static_small_stars = []
+    static_big_stars = []
+    dynamic_stars = []
+    nebulae = []
+    planets = []
+    is_any_small = False
+    is_any_big = False
+    is_any_dyn = False
+    is_any_neb = False
+    is_any_plan = False
+
+    for pair in objects_from_video:
+        frame_objects = pair[0]
+        frame_n = pair[1]
+
+        #prepare static stars and planets
+        if frame_n % frames_to_go_static == 0:
+            for star in frame_objects['stars']:
+                if star.diameter is None:
+                    is_any_small = True
+                    static_small_stars.append([star, frame_n])
+                else:
+                    is_any_big = True
+                    static_big_stars.append([star, frame_n])
+            for planet in frame_objects['planets']:
+                is_any_plan = True
+                planets.append([planet, frame_n])
+
+        #prepare dynamic stars
+        for star in frame_objects['stars_went_offscreen']:
+            is_any_dyn = True
+            dynamic_stars.append([star, frame_n])
+
+        #prepare nebulae
+        for point in frame_objects['nebulae']:
+            is_any_neb = True
+            nebulae.append([point, frame_n])
+
+
+
+    sss = SonificationTools(vid_w, vid_h, length, length*30)
+
+    chords_small_static = [["C4", "C5", "C6"]]
+    chords_big_static = [["A3", "A4", "C5"]]
+    chords_dynamic = [["A3", "A4", "E4", "C5"]]
+    chords_nebulae = [["A4", "A5", "C5", "E5", "C6"]]
+    chords_planets = [["C3", "E3", "C4", "E4"]]
+
+    #sonification
+    if is_any_small:
+        sss.sonificate_small_stars(sss.get_data_from_small_stars_in_list(static_small_stars, frames_to_go_static,100), chords_small_static, 0.4)
+    if is_any_big:
+        sss.sonificate_big_stars(sss.get_data_from_big_stars_in_list(static_big_stars, frames_to_go_static), chords_big_static, 0.3)
+    if is_any_dyn:
+        sss.sonificate_stars_went_offscreen(sss.get_data_from_dynamyc_stars_in_list(dynamic_stars, 1), chords_dynamic, 0.2)
+    if is_any_neb:
+        sss.sonificate_nebulae_point_list(sss.get_data_from_nebulas_in_list(nebulae), chords_nebulae, 0.1)
+    if is_any_plan:
+        sss.sonificate_planets(sss.get_data_from_planets_in_list(planets, frames_to_go_static), chords_planets, 0.01)
+
+    #mix audiostreams
+    sss.mix_all()
 
 class SonificationTools:
-    def __init__(self, vid_w, vid_h, length, chords=None):
-        if chords is None:
-            chords = [["A5", "C5", "C6"]]
-
+    def __init__(self, vid_w, vid_h, length, scale):
+        self.scale = scale
         self.vid_w = vid_w
         self.vid_h = vid_h
         self.length = length
-        self.chords = chords
         self.audio_system = "stereo"
         self.mapvals = {'phi': lambda x: [self.convert_x_to_phi(x_coord) for x_coord in x],
                         'theta': lambda x: [self.convert_y_to_theta(y_coord) for y_coord in x],
-                        'time': lambda x: (x-0)/(length-0),
+                        'time': lambda x: x,
                         'pitch': lambda x: x,
                         'volume': lambda x: x,
                         'time_evo': lambda x: x
@@ -37,6 +99,13 @@ class SonificationTools:
                         'volume': ('0', '100'),
                         'time_evo':('0', '100')
                         }
+
+    def get_h_fromrgb(self, rgb_point):
+        (h, s, v) = colorsys.rgb_to_hsv(rgb_point.r / 255, rgb_point.g / 255, rgb_point.b / 255)
+        return h
+
+    def scale_between(self, x, a, b, x_max, x_min):
+        return (b - a) * (x - x_min) / (x_max-x_min) + a
 
     def convert_x_to_phi(self, x_list):
         phi = []
@@ -76,12 +145,12 @@ class SonificationTools:
         for pair in objects:
             if pair[0].flux >= flux_filter:
                 smallStars.append(pair[0])
-                t.append((pair[0].x - pair[1] + static_time) / static_time)
+                t.append(pair[1] + (pair[0].x * self.scale / self.vid_w) % static_time)
 
         data["time"] = np.array(t)
         data["phi"] = np.array([star.x for star in smallStars])
         data["theta"] = np.array([star.y for star in smallStars])
-        data["pitch"] = np.array([star.color.r for star in smallStars])
+        data["pitch"] = np.array([self.get_h_fromrgb(star.color) for star in smallStars])
         data["volume"] = np.array([star.flux for star in smallStars])
 
         return data
@@ -91,8 +160,8 @@ class SonificationTools:
 
         data["phi"] = np.array([star[0].x for star in objects])
         data["theta"] = np.array([star[0].y for star in objects])
-        data["time"] = np.array([((star[0].x - star[1] + static_time) / static_time) for star in objects])
-        data["pitch"] = np.array([star[0].color.r for star in objects])
+        data["time"] = np.array([star[1] + (star[0].x * self.scale / self.vid_w) % static_time for star in objects])
+        data["pitch"] = np.array([self.get_h_fromrgb(star[0].color) for star in objects])
         data["volume"] = np.array([star[0].flux + star[0].diameter*10 for star in objects])
 
         return data
@@ -111,8 +180,11 @@ class SonificationTools:
                 data["phi"] = np.append(data["phi"], star.x)
                 data["theta"] = np.append(data["theta"], star.y)
                 data["time"] = np.append(data["time"], t)
-                data["pitch"] = np.append(data["pitch"], star.color.b)
-                data["volume"] = np.append(data["volume"], star.flux)
+                data["pitch"] = np.append(data["pitch"], self.get_h_fromrgb(star.color))
+                if star.diameter is None:
+                    data["volume"] = np.append(data["volume"], star.flux/2)
+                else:
+                    data["volume"] = np.append(data["volume"], star.flux*0.5+star.diameter*0.5)
 
         return data
 
@@ -129,11 +201,12 @@ class SonificationTools:
             idx = random.randint(idx, idx + step - 1) % len(list_of_nebulaes_data)
             point = list_of_nebulaes_data[idx][0]
             t = list_of_nebulaes_data[idx][1]
-            data["pitch"] = np.append(data["pitch"], point.color.b*0.5 + point.y*0.5)
+            data["pitch"] = np.append(data["pitch"], self.get_h_fromrgb(point.color) + point.y)
             data["phi"] = np.append(data["phi"], point.x)
             data["theta"] = np.append(data["theta"], point.y)
             data["time"] = np.append(data["time"], t)
             data["volume"] = np.append(data["volume"], abs(1 - (point.x/self.vid_w)**2))
+
 
         return data
 
@@ -147,14 +220,13 @@ class SonificationTools:
         }
 
         for planet, t in objects:
-            data["phi"] = np.append(data["phi"], (planet.x - t + static_time) / static_time)
+            data["phi"] = np.append(data["phi"], planet.x)
             data["theta"] = np.append(data["theta"], planet.y)
-            data["time"] = np.append(data["time"], t)
-            data["pitch"] = np.append(data["pitch"], planet.color.b)
+            data["time"] = np.append(data["time"], t + (planet.x * self.scale / self.vid_w) % static_time)
+            data["pitch"] = np.append(data["pitch"], self.get_h_fromrgb(planet.color))
             data["volume"] = np.append(data["volume"], planet.diameter)
 
         return data
-
 
     def sonificate_stars_went_offscreen(self, data, chords, volume=0.3):
         # ---------Sources----------
@@ -175,7 +247,6 @@ class SonificationTools:
         sonification = Sonification(score, source, generator, self.audio_system)
         sonification.render()
         sonification.save("out/stars_went_offscreen.wav", volume)
-        sonification.notebook_display()
 
     def sonificate_small_stars(self, data, chords, volume=0.5):
         # ---------Sources----------
@@ -196,7 +267,6 @@ class SonificationTools:
         sonification = Sonification(score, source, generator, self.audio_system)
         sonification.render()
         sonification.save("out/static_small_stars.wav", volume)
-        sonification.notebook_display()
 
     def sonificate_big_stars(self, data, chords, volume=0.4):
         # ---------Sources----------
@@ -217,7 +287,6 @@ class SonificationTools:
         sonification = Sonification(score, source, generator, self.audio_system)
         sonification.render()
         sonification.save("out/static_big_stars.wav", volume)
-        sonification.notebook_display()
 
     def sonificate_nebulae_point_list(self, data, chords, volume=0.25):
         # ---------Sources---------
@@ -238,7 +307,6 @@ class SonificationTools:
         sonification = Sonification(score, source, generator, self.audio_system)
         sonification.render()
         sonification.save("out/nebulae.wav", volume)
-        sonification.notebook_display()
 
     def sonificate_windy(self, data = None):
         data = {
@@ -270,8 +338,6 @@ class SonificationTools:
         sonification = Sonification(score, source, generator, self.audio_system)
         sonification.render()
         sonification.save("out/windy.wav", 0.005)
-        sonification.notebook_display()
-        sonification.save_combined()
 
     def sonificate_planets(self, data, chords, volume=0.5):
         # ---------Sources----------
@@ -292,7 +358,7 @@ class SonificationTools:
         sonification = Sonification(score, source, generator, self.audio_system)
         sonification.render()
         sonification.save("out/planets.wav", volume)
-        sonification.notebook_display()
+
 
     def mix_2_wavs(self, file_path1, file_path2, file_path_export):
         file1 = AudioSegment.from_wav(file_path1)
