eff1d2ebe7df87bb6a05b574178e42353f542568
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2023-10-08 16:58:56 +0300

    fixed nebula recognition

--- Nebulae.py
+++ Nebulae.py
@@ -21,13 +21,12 @@ class Nebulae:
 
 
 def find_nebulae_and_planets(image, gray_image):
-    _, thresholded_frame = cv2.threshold(gray_image, 1, 255, cv2.THRESH_BINARY)
+    _, thresholded_frame = cv2.threshold(gray_image, 5, 255, cv2.THRESH_BINARY)
     # cv2.CHAIN_APPROX_SIMPLE CHAIN_APPROX_TC89_KCOS RETR_TREE RETR_LIST cv2.RETR_CCOMP cv2.RETR_EXTERNAL
-    contours, hierarchy = cv2.findContours(thresholded_frame, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_KCOS)
+    contours, hierarchy = cv2.findContours(thresholded_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)
     nebulae = Nebulae()
     planets = []
     for i, contour in enumerate(contours):
-        polinomial = cv2.approxPolyDP(contour, epsilon=18, closed=True)
         is_planet = False
         # check for planet
         if hierarchy[0][i][3] == -1:
@@ -35,7 +34,8 @@ def find_nebulae_and_planets(image, gray_image):
             area = cv2.contourArea(contour)
             if perimeter > 0 and area > 100 and area < 4000:
                 roundness = (4 * np.pi * area) / perimeter ** 2
-                if roundness > 0.6:
+                print(roundness)
+                if roundness > 0.55:
                     is_planet = True
                     x, y, w, h = cv2.boundingRect(contour)
                     acc_color = np.array([0, 0, 0])
@@ -47,7 +47,10 @@ def find_nebulae_and_planets(image, gray_image):
                     avg_color = acc_color / npix
                     planet =Planet(round(x+w/2), round(y+h/2), max(w, h), RGBColor(avg_color[2], avg_color[1], avg_color[0]))
                     planets.append(planet)
-        if not is_planet and polinomial.shape[0] > 2:
+
+        if not is_planet:
+            polinomial = cv2.approxPolyDP(contour, epsilon=18, closed=True)
+            if polinomial.shape[0] > 2:
                 polinomial = polinomial.astype('int32')
                 polinomial = polinomial.reshape((polinomial.shape[0], 2))
                 nebulae.contours.append(polinomial)
@@ -55,4 +58,5 @@ def find_nebulae_and_planets(image, gray_image):
                     nebulae.colorful_points.append(ColorfulPoint(pt[0], pt[1], RGBColor(image[pt[1], pt[0], 2],
                                                                                 image[pt[1], pt[0], 1],
                                                                                 image[pt[1], pt[0], 0])))
+
     return nebulae, planets
